b85ae7000fbafb64c12afbfbea4badb80f473220
colinxu2020 <63941938+colinxu2020@users.noreply.github.com>
2022-09-10 18:13:04 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,20 +17,22 @@ class Settings(object):
         try:
             cls.load_settings()
         except FileNotFoundError:
-            cls.settings = cls.ask_user_settings()
+            cls.settings = cls.ask_settings()
             cls.update_settings()
-    name = ['声音','报错重启','已关闭此设置','功能时差','用户名']
+
+    name = ['报错重启','功能时差','用户名']
     
     @staticmethod
     def ask_settings() -> list:
-        return [1,1,-1,0.6,input('Please input your username (will be saved):')]
+        return [int(input('是否启用报错自动重启？')),float(input('请输入功能时差：')),input('请输入用户名:')]
         
     @classmethod
     def ask_and_update_single_settings(cls, idx) -> None:
         idx-=1
         print(cls.name[idx])
         print('目前状态:',cls.settings[idx])
-        if idx!=4: cls.settings[idx] = input('输入新值:')
+        if idx!=2: cls.settings[idx] = input('输入新值:')
         else: cls.settings[idx] = int(input('输入新值:'))
         
+
         
