b93bc28c6167e0aa3543309e2e38c463062fa03b
Colinxu2020 <colinxu2020@gmail.com>
2022-10-02 20:26:25 +0800

    UPD

---
+++ categories/dgncx.py
@@ -0,0 +1,21 @@
+import os
+
+
+import categorybase as base
+import dgncx
+
+
+class Dgncx(base.BaseCategory):
+    """Dgncx相关功能"""
+    def calculator():
+        """计算器"""
+        dgncx.calculator()
+    def game():
+        """游戏"""
+        dgncx.calculator()
+    def solution():
+        """题解"""
+        dgncx.solution()
+    def system():
+        """系统"""
+        dgncx.system()
